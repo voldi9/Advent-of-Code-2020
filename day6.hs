47a548e54ea355edc1e5298943ad54f805769f70
Filip Kowalski <fkow@google.com>
2020-12-23 06:41:56 +0100

    Day 6 problem solution nit

--- day6.hs
+++ day6.hs
@@ -4,5 +4,6 @@ import qualified Data.Set as Set
 main = do
   contents <- getContents
   print . sum $ map (length . Set.fromList . filter isLower) (splitOnBlankLine contents)
-  print . sum $ map (length . \s -> filter (allRows . splitOnNewline $ s) ['a'..'z']) (splitOnBlankLine contents) where
+  print . sum $ map (length . hasLetters) (splitOnBlankLine contents) where
+    hasLetters s = filter (allRows . splitOnNewline $ s) ['a'..'z']
     allRows r c = and $ map (elem c) r
